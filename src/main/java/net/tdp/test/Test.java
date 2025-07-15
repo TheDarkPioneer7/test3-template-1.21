8a408c2a4b1700ec551c4acf2dbf1d957bc2e250
TheDarkPioneer <jakobsenjakob07@gmail.com>
2025-07-15 15:35:18 +0200

    blupp

--- src/main/java/net/tdp/test/Test.java
+++ src/main/java/net/tdp/test/Test.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 
 public class Test implements ModInitializer {
 	public static final String MOD_ID = "test";
-
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
