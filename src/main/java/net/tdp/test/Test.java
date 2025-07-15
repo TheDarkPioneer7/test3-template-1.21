ce8423997862778dcaccd1d0d53487ece60a2306
TheDarkPioneer <jakobsenjakob07@gmail.com>
2025-07-15 15:32:54 +0200

    Initial commit

---
+++ src/main/java/net/tdp/test/Test.java
@@ -0,0 +1,19 @@
+package net.tdp.test;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test implements ModInitializer {
+	public static final String MOD_ID = "test";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+
+	}
+}
---
+++ src/main/java/net/tdp/test/TestClient.java
@@ -0,0 +1,10 @@
+package net.tdp.test;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
