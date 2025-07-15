9a120e9913ed25925992e2f73bc95821f6df4c78
TheDarkPioneer <jakobsenjakob07@gmail.com>
2025-07-16 00:35:34 +0200

    non-cubic blocks addition

--- src/main/java/net/tdp/test/TestClient.java
+++ src/main/java/net/tdp/test/TestClient.java
@@ -1,10 +1,14 @@
 package net.tdp.test;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import net.tdp.test.block.ModBlocks;
 
 public class TestClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PINK_GARNET_DOOR, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PINK_GARNET_TRAPDOOR, RenderLayer.getCutout());
     }
 }
